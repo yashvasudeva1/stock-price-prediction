60e201f56d54ab2d74a01d8a349087ba4d6edbdd
Yash Vasudeva <117675995+yashvasudeva1@users.noreply.github.com>
2025-11-28 16:57:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,8 +129,9 @@ elif page == "EDA":
     # Top metrics
     col1, col2, col3 = st.columns(3)
     col1.metric("Total Rows", summary["total_rows"])
-    col2.metric("Start Date", str(summary["start_date"]))
-    col3.metric("End Date", str(summary["end_date"]))
+    col2.write(f"**Start Date:**  {summary['start_date']}")
+    col3.write(f"**End Date:**  {summary['end_date']}")
+
 
 
     # Columns list
